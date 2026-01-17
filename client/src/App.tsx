a7f803416364e3437c43edb39565705afd7c4c00
shevtsov-dmitry <shadowstakeus28@gmail.com>
2026-01-17 12:49:40 +0300

    refactor: reinit client with vite build system

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import {type ReactElement} from 'react'
+import './App.css'
+
+const App = (): ReactElement => {
+
+    return (
+        <>
+            <h1 className={"text-red-500"}>app</h1>
+        </>
+    )
+}
+
+export default App
