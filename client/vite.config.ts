ca79727cbd5b5289bf97c9804914fc5354c7c205
dmitry-shevtsov <shadowstakeus28@gmail.com>
2026-02-24 11:34:16 +0300

    cicd: remove base paths for gitlab pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/habit-lens/',
+    base: process.env.VITE_BASE_PATH || '/',
     plugins: [
         tailwindcss(),
         react({
